235a9f25c2d1725b646f4c8c80e621b6f14d3b60
Tomoki Yamashita <yamashita@hasegawa-elec.co.jp>
2022-03-01 09:49:55 +0900

    MVVM実装 https://qiita.com/Zuishin/items/6e67d77252ea5c0e1cc8#windows-forms

---
+++ ViewModelWFApp1/Command.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ViewModelWFApp1 {
+    public class Command : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public PropertySetter PropertySetter { get; private set; }
+
+        public Command(Action execute) {
+            PropertySetter = new PropertySetter(this, OnPropertyChanged);
+            this.execute = execute;
+        }
+
+        public Command(Action execute, bool canExecute)
+            : this(execute) {
+            CanExecute = canExecute;
+        }
+
+        public Command(Action execute, IObservable<bool> canExecute)
+            : this(execute) {
+            canExecute.Subscribe(a => CanExecute = a);
+        }
+
+        private Action execute;
+        public void Execute() {
+            if (!CanExecute) return;
+            execute?.Invoke();
+        }
+
+        private bool canExecute;
+        public bool CanExecute {
+            get => canExecute;
+            set => PropertySetter.Set(ref canExecute, value);
+        }
+    }
+
+    public static class CommandExtension {
+        public static Command ToCommand(this IObservable<bool> source, Action execute = null) {
+            return new Command(execute, source);
+        }
+    }
+}
